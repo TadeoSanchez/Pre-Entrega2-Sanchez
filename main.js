244c81f76528e66eed3ce3ad5be44de832242d14
Tadeo Sánchez <tadeosanchez125@gmail.com>
2023-11-13 20:47:17 -0300

    commit final

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 alert("Bienvenido al programa para cargar nombres, verlos y eliminarlos");
+// CREACIÓN DE LA CLASE PERSONA
 class Persona {
   constructor(nombre, edad, nacimiento) {
     this.Nombre = nombre;
@@ -6,11 +7,10 @@ class Persona {
     this.Nacimiento = nacimiento;
   }
 }
-const persona1 = new Persona("Ariel", 19, "18-02-2004");
-const persona2 = new Persona("Britos", 20, "23-09-2003");
-const persona3 = new Persona("Tadeo", 19, "25-09-2003");
-const arrayPersonas = [persona1, persona2, persona3];
 
+const arrayPersonas = []; // ARRAY VACÍO PARA PODER AÑADIR LOS OBJETOS
+
+// FUNCIÓN PARA CARGAR LOS NOMBRES AL ARRAY
 const cargarNombres = () => {
   let ingresarNombre = prompt("Ingrese el nombre");
   let ingresarEdad = parseInt(prompt("Ingrese la edad"));
@@ -29,13 +29,16 @@ const cargarNombres = () => {
       `Persona cargada exitosamente \nNombre: ${nuevaPersona.Nombre}\nEdad: ${nuevaPersona.Edad}\nNacimiento: ${nuevaPersona.Nacimiento}`
     );
   } else {
+    alert(`Debe ingresar todos los datos`);
     console.log(`Debe ingresar todos los datos`);
   }
 };
 
+// FUNCIÓN PARA MOSTRAR LOS NOMBRES DENTRO DEL ARRAY
 const mostrarNombres = () => {
   if (arrayPersonas.length === 0) {
-    console.log("Primero ingrese datos para continuar");
+    alert("Primero ingrese datos para poder visualizarlos");
+    console.log("Primero ingrese datos para poder visualizarlos");
   } else {
     let mensaje = "Datos cargados:";
     arrayPersonas.forEach((persona, index) => {
@@ -44,9 +47,59 @@ const mostrarNombres = () => {
       }\nNacimiento: ${persona.Nacimiento}\n----------------------------`;
     });
     alert(mensaje);
+    console.log(mensaje);
   }
 };
 
+// FUNCIÓN PARA ELIMINAR LOS ELEMENTOS DENTRO DEL ARRAY DE DIFERENTES MANERAS
+const eliminarDatos = () => {
+  let eleccion = parseInt(
+    prompt(
+      "Elija una de las siguientes opciones para eliminar\n1-Eliminar primer usuario creado\n2-Eliminar último usuario creado\n3-Eliminar usuario basado en su número de persona\n4-Eliminar todo"
+    )
+  );
+
+  switch (eleccion) {
+    case 1:
+      arrayPersonas.shift();
+      alert(`Primer usuario eliminado con éxito`);
+      console.table(arrayPersonas);
+      break;
+    case 2:
+      arrayPersonas.pop();
+      alert(`Último usuario eliminado con éxito`);
+      console.table(arrayPersonas);
+      break;
+    case 3:
+      let numeroDePersona =
+        parseInt(prompt("Ingrese el número de la persona que desea eliminar")) -
+        1;
+      if (
+        isNaN(numeroDePersona) ||
+        numeroDePersona < 0 ||
+        numeroDePersona >= arrayPersonas.length
+      ) {
+        alert("Número de persona invalido, ingrese uno que exista");
+        console.log("Número de persona invalido, ingrese uno que exista");
+      } else {
+        arrayPersonas.splice(numeroDePersona, 1);
+        alert(`Se ha eliminado a la persona N°${numeroDePersona + 1}`);
+        console.table(arrayPersonas);
+      }
+      break;
+    case 4:
+      arrayPersonas.splice(0);
+      alert(`Se han eliminado todos los usuarios`);
+      console.log(arrayPersonas);
+      break;
+    default:
+      alert("Opción invalida");
+      console.log("Opción no valida");
+      break;
+  }
+};
+
+// FUNCIÓN PARA EL MENÚ DEL PROGRAMA
 function menu() {
   let menu = prompt(
     "Ingrese una opción (`Ingrese ESC para finalizar`)\n1-Cargar Nombres y Datos\n2-Ver Nombres y Datos\n3-Eliminarlos"
@@ -54,15 +107,16 @@ function menu() {
   while (menu.toUpperCase() !== "ESC") {
     switch (parseInt(menu)) {
       case 1:
-        console.log("Elegiste la opción 1");
+        cargarNombres();
         break;
       case 2:
-        console.log("Elegiste la opción 2");
+        mostrarNombres();
         break;
       case 3:
-        console.log("Elegiste la opción 1");
+        eliminarDatos();
         break;
       default:
+        alert("Elija la opción correcta");
         console.log("Elija la opción correcta");
         break;
     }
@@ -70,8 +124,7 @@ function menu() {
       "Ingrese una opción (`Ingrese ESC para finalizar`)\n1-Cargar Nombres y Datos\n2-Ver Nombres y Datos\n3-Eliminarlos"
     );
   }
+  alert(`Programa finalizado, muchas gracias por utilizarlo :D`);
 }
 
-//cargarNombres();
-//menu();
-mostrarNombres();
+menu();
